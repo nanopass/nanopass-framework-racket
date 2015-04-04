63a825bcb81da2779e427412d752675216f4e5b9
Leif Andersen <leif@leifandersen.net>
2015-04-04 17:09:30 -0400

    Add dependencies.

--- info.rkt
+++ info.rkt
@@ -1,3 +1,8 @@
 #lang info
 (define version "0.1")
 (define collection "nanopass")
+
+(define deps '("base"
+               "rackunit-lib"))
+(define build-deps '("scribble-lib"
+                     "racket-doc"))
